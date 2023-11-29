5c0636a6385a309044d9577a30bf2fc399bfa11d
Michał Doś <93281456+michaaalD@users.noreply.github.com>
2023-11-29 16:58:05 +0100

    Update test.c

--- test.c
+++ test.c
@@ -1,50 +1,70 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/mman.h>
 #include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <pthread.h>
 
-#define BABBLE_NAME "/Y01-42e"
+#define BABBLE_NAME "kkucz"
 #define BABBLE_MODE 0777
 #define BABBLE_LIMIT 32
 #define BABBLE_LENGTH 80
 
-struct babblespace {
-  pthread_mutex_t babble_mutex;
-  pthread_cond_t babble_cond;
-  int babble_first, babble_total;
-  char babbles[BABBLE_LIMIT][BABBLE_LENGTH];
+
+struct babblespace
+{
+    pthread_mutex_t babble_mutex;
+    pthread_cond_t babble_cond;
+    int babble_first, babble_total;
+    char babbles[BABBLE_LIMIT][BABBLE_LENGTH];
 };
 
-int main() {
-  int fd = shm_open(BABBLE_NAME, O_RDWR, BABBLE_MODE);
-  if (fd == -1) {
-    perror("shm_open");
-    exit(EXIT_FAILURE);
-  }
+void wyswietl(struct babblespace *babble_wsk)
+{
+    pthread_mutex_lock(&babble_wsk->babble_mutex);
+    printf("Komunikaty w pamięci:\n");
+    int i = babble_wsk->babble_first;
+    int licznik = 0;
 
-  struct babblespace *babbles = mmap(NULL, sizeof(struct babblespace), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-  if (babbles == MAP_FAILED) {
-    perror("mmap");
-    exit(EXIT_FAILURE);
-  }
+    while (licznik < babble_wsk->babble_total)
+    {
+        printf("[%d]: %s\n", i + 1, babble_wsk->babbles[i]);
+        i = (i + 1) % BABBLE_LIMIT;
+        licznik++;
+    }
+    pthread_mutex_unlock(&babble_wsk->babble_mutex);
+}
 
-  for (int i = 0; i < babbles->babble_total; i++) {
-    printf("%s\n", babbles->babbles[i]);
-  }
+int main()
+{
+    int shmd;
+    struct babblespace *babble_wsk;
+    shmd = shm_open(BABBLE_NAME, O_RDWR, BABBLE_MODE);
+    if (shmd == -1)
+    {
+        perror("Błąd przy otwieraniu");
+        exit(1);
+    }
 
-  if (munmap(babbles, sizeof(struct babblespace)) == -1) {
-    perror("munmap");
-    exit(EXIT_FAILURE);
-  }
+    ftruncate(shmd, sizeof(struct babblespace));
 
-  if (close(fd) == -1) {
-    perror("close");
-    exit(EXIT_FAILURE);
-  }
+    babble_wsk = (struct babblespace *)mmap(NULL, sizeof(struct babblespace), PROT_READ | PROT_WRITE, MAP_SHARED, shmd, 0);
+    if (babble_wsk == MAP_FAILED)
+    {
+        perror("Błąd przy mapowaniu");
+        exit(1);
+    }
 
-  return 0;
+    wyswietl(babble_wsk);
+    munmap(babble_wsk, sizeof(struct babblespace));
+
+    if (close(shmd) == -1)
+    {
+        perror("Błąd przy zamykaniu");
+        exit(1);
+    }
+
+    return 0;
 }
